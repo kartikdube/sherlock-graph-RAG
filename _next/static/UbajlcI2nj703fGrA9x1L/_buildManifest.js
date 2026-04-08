f57cbfc2f60c68ed98e4d61f42719b639211c34f
Kartik Dube <dubekartik1@gmail.com>
2026-04-08 23:24:07 +0200

    Deploy static build to sherlock-graph-RAG-static

---
+++ _next/static/UbajlcI2nj703fGrA9x1L/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/sherlock-graph-RAG//_next/:path+",
+        "destination": "/sherlock-graph-RAG/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
